78ce46cb1000eeb37c3faaff90b48efa7ce3dcb4
wothag <d.r.c.hagnere@gmail.com>
2018-06-14 21:28:56 +0200

    install doctrine

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,32 +8,26 @@
 
 namespace App\Controller;
 
-
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends AbstractController
 {
+
     /**
      * @Route("/")
      */
-    public function homepage()
+    public function homepage(Request $request)
     {
-
+       $session = $request->getSession();
+       $session->set("id", "8");
        return $this->render('homepage/home.html.twig', [
         'title'=>'Accueil'
-
 
        ]) ;
     }
 
-    /**
-     * @Route("/reservation")
-     */
-    public function show()
-    {
-            return $this->render('reservation/show.html.twig');
-    }
 }
 
